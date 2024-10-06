94b4896c9f8aefa2155652e6b64a785822eaf253
Rusenshi <jeremiasjulian5003@gmail.com>
2024-10-06 12:42:41 -0300

    nueva interfaz del dao usada: tiene un tipo de objeto, y un tipo para la clave (asi podemos usar Integer o String dependiendo del objeto en cuestion)

--- src/main/java/com/mycompany/helloworld/ejemplopostgresql/DAOPacienteSQL.java
+++ src/main/java/com/mycompany/helloworld/ejemplopostgresql/DAOPacienteSQL.java
@@ -4,6 +4,10 @@
  */
 package com.mycompany.helloworld.ejemplopostgresql;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,31 +15,123 @@ import java.util.Optional;
  *
  * @author jerem
  */
-public class DAOPacienteSQL implements DaoOld<Paciente>{
+public class DAOPacienteSQL implements Dao<Paciente,Integer>{
 
     @Override
-    public Optional<Paciente> get(long id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public Optional<Paciente> get(Integer id) {
+    try{
+            // Crea lista temporal para recorrer resultset
+            List<Paciente> pacientes = new ArrayList<>();
+            
+            // Preparar Consulta
+            // Nota: Activo es la variable para el eliminado logico
+            PreparedStatement stm = DataBaseSingleton.getInstance().getConnection().prepareStatement("SELECT * FROM PACIENTES WHERE activo=1 AND dni=?");
+            stm.setInt(1, id);
+            ResultSet res = stm.executeQuery();   
+            
+            // Recorrer resultado de la consulta y convertirlo en una lista
+            while(res.next()){
+                // Obtiene los campos de cada columna de la base de datos y crea el objeto.
+                pacientes.add(new Paciente(res.getString(2),res.getString(3),res.getInt(1),res.getInt(4),res.getInt(5),res.getString(6),res.getString(7),res.getString(8)));
+            }
+            
+            // Comprueba si encontró al paciente de dni "id"
+            if (pacientes.isEmpty()) return Optional.empty();
+            
+            // Retorna el unico elemento que debería haber en la lista
+            return Optional.ofNullable(pacientes.get((int) 0)); 
+        }
+        catch(SQLException e){
+            System.out.println("Error al buscar por clave");
+            System.out.println(e.toString());
+            return Optional.empty();
+        }
     }
 
     @Override
     public List<Paciente> getAll() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        // Crea lista temporal para recorrer resultset
+        List<Paciente> pacientes = new ArrayList<>();
+        
+        try{
+            // Preparar Consulta
+            // Nota: Activo es la variable para el eliminado logico
+            PreparedStatement stm = DataBaseSingleton.getInstance().getConnection().prepareStatement("SELECT * FROM PACIENTES WHERE activo=1");
+            ResultSet res = stm.executeQuery();
+            
+            // Recorrer resultado de la consulta y convertirlo en una lista
+            while(res.next()){
+                // Obtiene los campos de cada columna de la base de datos y crea el objeto.
+                pacientes.add(new Paciente(res.getString(2),res.getString(3),res.getInt(1),res.getInt(4),res.getInt(5),res.getString(6),res.getString(7),res.getString(8)));
+            }
+        }
+        catch(SQLException e){
+            System.out.println("Error al obtener todos");
+            System.out.println(e.toString());
+        }
+        
+        return pacientes;
     }
 
     @Override
     public void save(Paciente t) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try{
+            // Preparar Insercion
+            // Nota: Activo es la variable para el eliminado logico
+            PreparedStatement stm = DataBaseSingleton.getInstance().getConnection().prepareStatement("INSERT INTO PACIENTES VALUES (?,?,?,?,?,?,?,?,1);");
+            stm.setInt(1, t.getDni());
+            stm.setString(2, t.getNombre());
+            stm.setString(3, t.getApellido());
+            stm.setInt(4, t.getSexo());
+            stm.setInt(5, t.getEdad());
+            stm.setString(6, t.getDomicilio());
+            stm.setString(7, t.getTelefono());
+            stm.setString(8, t.getCorreoElectronico());
+            
+            stm.execute();
+        }
+        catch(SQLException e){
+            System.out.println("Error al insertar");
+            System.out.println(e.toString());
+        }
     }
 
     @Override
-    public void update(Paciente t, Object[] params) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void update(Paciente t) {
+        try{
+            // Preparar Modificación
+            // Nota: Activo es la variable para el eliminado logico
+            PreparedStatement stm = DataBaseSingleton.getInstance().getConnection().prepareStatement("UPDATE PACIENTES SET dni=?,nombre=?,apellido=?,sexo=?,edad=?,domicilio=?,telefono=?,correoElectronico=? WHERE activo = 1 AND dni=?;");
+            stm.setInt(1, t.getDni());
+            stm.setString(2, t.getNombre());
+            stm.setString(3, t.getApellido());
+            stm.setInt(4, t.getSexo());
+            stm.setInt(5, t.getEdad());
+            stm.setString(6, t.getDomicilio());
+            stm.setString(7, t.getTelefono());
+            stm.setString(8, t.getCorreoElectronico());
+            stm.setInt(9, t.getDni());
+            
+            stm.execute();
+        }
+        catch(SQLException e){
+            System.out.println("Error al modificar");
+            System.out.println(e.toString());
+        }
     }
 
     @Override
-    public void delete(Paciente t) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void delete(Integer id) {
+        try{
+            // Preparar Eliminacion (Logica)
+            PreparedStatement stm = DataBaseSingleton.getInstance().getConnection().prepareStatement("UPDATE PACIENTES SET activo=0 WHERE dni=?");
+            stm.setInt(1, id);
+
+            stm.execute();
+        }
+        catch(SQLException e){
+            System.out.println("Error al eliminar");
+            System.out.println(e.toString());
+        }
     }
-    
 }
